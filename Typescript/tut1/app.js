caeb7f99d3c5159dfb698cfc47c80681b9fac6e5
patelsachin344 <sachinpatel71344@gmail.com>
2023-04-02 18:51:16 +0530

    mis

--- Typescript/tut1/app.js
+++ Typescript/tut1/app.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 var a = "sachin";
 // console.log(a)
@@ -24,7 +39,6 @@ var d = 5;
 var e = [1, 2, 3, 4, 5, "vf", false];
 e.push("ds");
 e.push(true);
-console.log(e);
 var obj = {
     name: "sachin",
     age: 23,
@@ -37,4 +51,59 @@ var obj1 = {
 };
 // obj.name = "patel"
 // obj.name = 12 error
-console.log(obj);
+// console.log(obj)
+var f = "any";
+f = 23;
+f = [];
+var g = [];
+var h = {
+    name: "no name",
+    age: 30,
+    print: function () {
+        return this.name;
+    }
+};
+// console.log(h.print())
+// function add (a,b):number {
+//     return a+b
+// }
+// console.log(typeof add(1 , "D")) // string
+function add(a, b) {
+    return b ? a + b : a;
+}
+// console.log( add(1 ))
+var App = /** @class */ (function () {
+    function App(name) {
+        this.fname = "fname";
+        this.fname = name;
+    }
+    App.prototype.getprint = function (age) {
+        console.log(this.fname + age);
+    };
+    return App;
+}());
+var app = new App("sachin");
+// app.getprint(23)
+// inheritance 
+var Parent = /** @class */ (function () {
+    function Parent() {
+    }
+    Parent.prototype.setName = function (name) {
+        return this.name = name;
+    };
+    return Parent;
+}());
+var Child = /** @class */ (function (_super) {
+    __extends(Child, _super);
+    function Child() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    // name="child";
+    Child.prototype.getName = function () {
+        return this.name;
+    };
+    return Child;
+}(Parent));
+var child = new Child();
+child.setName("from parent");
+console.log(child.getName());
--- Typescript/tut1/app.ts
+++ Typescript/tut1/app.ts
@@ -28,7 +28,7 @@ let e = [1,2,3,4,5,"vf",false]
 e.push("ds")
 e.push(true)
 
-console.log(e)
+// console.log(e)
 
 interface objType {
     name: string,
@@ -49,4 +49,65 @@ let obj1:any = {
 // obj.name = "patel"
 // obj.name = 12 error
 
-console.log(obj)
+// console.log(obj)
+
+let f:string|number|[] = "any"
+f = 23
+f = []
+let g:{}= []
+
+interface hType {
+    name:string,
+    age:number,
+    print:()=>{}
+}
+let h:hType = {
+    name:"no name",
+    age:30,
+    print:function (){
+        return this.name
+    }
+}
+// console.log(h.print())
+
+// function add (a,b):number {
+//     return a+b
+// }
+// console.log(typeof add(1 , "D")) // string
+function add (a,b?):number {
+    return b? a+b :a
+}
+// console.log( add(1 ))
+
+
+class App {
+    fname = "fname"
+    constructor(name:string){
+        this.fname = name
+    }
+    getprint(age:number){
+        console.log(this.fname + age)
+    }
+}
+const app = new App("sachin")
+// app.getprint(23)
+
+
+// inheritance 
+
+class Parent {
+    name;
+    setName(name:string):string{
+        return this.name = name
+    }
+}
+class Child extends Parent{
+    // name="child";
+    getName():string{
+        return this.name
+    }
+}
+
+let child = new Child()
+child.setName("from parent")
+console.log(child.getName())
